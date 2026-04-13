ddd587936046ac76d1c54c065101b0e13a0e7459
Augusto-LJ <augustolimajardim@gmail.com>
2026-04-13 20:59:33 -0300

    feat: cria enums e classes base para entities, value objects, clause guards e domain exceptions

---
+++ Vendas.Domain/Common/Base/Entity.cs
@@ -0,0 +1,52 @@
+﻿namespace Vendas.Domain.Common.Base;
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+    public DateTime DataCriacao { get; protected set; }
+    public DateTime? DataAtualizacao { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        DataCriacao = DateTime.UtcNow;
+    }
+
+    protected void SetDataAtualizacao()
+    {
+        DataAtualizacao = DateTime.UtcNow;
+    }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity left, Entity right)
+    {
+        if (left is null)
+            return right is null;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity left, Entity right)
+    {
+        return !(left == right);
+    }
+}
---
+++ Vendas.Domain/Common/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Vendas.Domain.Common.Exceptions;
+public class DomainException(string message) : Exception(message)
+{
+    public static void When(bool hasError, string errorMessage)
+    {
+        if (hasError)
+            throw new DomainException(errorMessage);
+    }
+}
