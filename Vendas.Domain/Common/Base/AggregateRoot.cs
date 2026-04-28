9941e5fd7aaa4bd34ab944cbcddac7758e52742d
Augusto-LJ <augustolimajardim@gmail.com>
2026-04-28 19:12:07 -0300

    feat: cria classe abstrata AggregateRoot e o VO MotivoCancelamento

---
+++ Vendas.Domain/Common/Base/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Vendas.Domain.Common.Base;
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot() : base()
+    {
+    }
+
+    protected AggregateRoot(Guid id) : base(id)
+    {
+    }
+}
---
+++ Vendas.Domain/ValueObjects/MotivoCancelamento.cs
@@ -0,0 +1,44 @@
+﻿using Vendas.Domain.Common.Base;
+using Vendas.Domain.Common.Exceptions;
+using Vendas.Domain.Common.Validations;
+
+namespace Vendas.Domain.ValueObjects;
+public sealed class MotivoCancelamento : ValueObject
+{
+    public string Codigo { get; }
+    public string Descricao { get; }
+
+    private static readonly Dictionary<string, string> _motivosCancelamento = new()
+    {
+        { "ClienteDesistiu", "Cliente desistiu da compra" },
+        { "ErroProcessamentoPagamento", "Erro no processamento do pagamento" },
+        { "ItemEsgotadoEstoque", "Item esgotado no estoque" },
+        { "EnderecoEntregaInvalido", "Endereço de entrega inválido" },
+        { "Outro", "Outro motivo não especificado" }
+    };
+
+    public MotivoCancelamento(string codigo)
+    {
+        Guard.AgainstNullOrWhiteSpace(codigo, nameof(codigo));
+
+        if (!_motivosCancelamento.TryGetValue(codigo, out string? value))
+            throw new DomainException($"Motivo de cancelamento inválido: {codigo}");
+
+        Codigo = codigo;
+        Descricao = value;
+    }
+
+    public static MotivoCancelamento ClienteDesistiu => new("ClienteDesistiu");
+    public static MotivoCancelamento ErroProcessamentoPagamento => new("ErroProcessamentoPagamento");
+    public static MotivoCancelamento ItemEsgotadoEstoque => new("ItemEsgotadoEstoque");
+    public static MotivoCancelamento EnderecoEntregaInvalido => new("EnderecoEntregaInvalido");
+    public static MotivoCancelamento Outro => new("Outro");
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Codigo;
+        yield return Descricao;
+    }
+
+    public override string ToString() => $"{Descricao}";
+}
