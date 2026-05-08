93546f3705b008758154e3d14c5b5f602b98ef3b
Augusto-LJ <augustolimajardim@gmail.com>
2026-05-08 09:49:20 -0300

    feat: cria o caso de uso AdicionarItemAoPedido

---
+++ Vendas.Application/Commands/Pedidos/AdicionarItemAoPedido/AdicionarItemAoPedidoResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Vendas.Application.Commands.Pedidos.AdicionarItemAoPedido;
+public sealed class AdicionarItemAoPedidoResultDto(Guid PedidoId, decimal ValorTotal, string Status)
+{
+    public Guid PedidoId { get; } = PedidoId;
+    public decimal ValorTotal { get; } = ValorTotal;
+    public string Status { get; } = Status;
+}
+
