301ddbbae155a64d82e7ea2df3e7268e603a9286
Augusto-LJ <augustolimajardim@gmail.com>
2026-05-08 09:35:15 -0300

    feat: Cria caso de uso CriarPedido

---
+++ Vendas.Application/Commands/Pedidos/CriarPedido/CriarPedidoCommand.cs
@@ -0,0 +1,8 @@
+﻿using Vendas.Domain.Pedidos.ValueObjects;
+
+namespace Vendas.Application.Commands.Pedidos.CriarPedido;
+public sealed class CriarPedidoCommand(Guid clienteId, EnderecoEntrega enderecoEntrega)
+{
+    public Guid ClienteId { get; } = clienteId;
+    public EnderecoEntrega EnderecoEntrega { get; } = enderecoEntrega;
+}
